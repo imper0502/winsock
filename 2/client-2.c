480600d95b92b14dca25b4952ec8e20d585c5537
imper0502 <imper0502@outlook.com>
2016-04-02 23:10:07 +0800

    完成練習二 更新讀我檔

--- 2/client-2.c
+++ 2/client-2.c
@@ -12,28 +12,36 @@ void main() {
 	SOCKET sd;
 	struct sockaddr_in serv;
 	
-	int n;
+	u_short port = 4321;
+	int n = 0;
 	char str[MAXLINE];
 	/**初始化區****************************************/ 
 	WSAStartup(0x101, &wsadata);
-	
 	sd = socket(AF_INET, SOCK_STREAM, 0);
 	
 	serv.sin_family = AF_INET;
-	serv.sin_port = htons(4321);
+	serv.sin_port = htons(port);
 	serv.sin_addr.s_addr = inet_addr("127.0.0.1");
+	/**改port功能**/ 
+	printf("INPUT PORT(default:4321) >> ");
+	gets(str);
+	if(strlen(str)!=0) {
+		port=(u_short)atoi(str);
+		if(port != (u_short)4321)serv.sin_port = htons(port);
+	}
 	
 	/**工作區*****************************************/ 
-	connect(sd, (LPSOCKADDR)&serv, sizeof(serv));
+	printf("Client: waiting for server...\n");
+	while(connect(sd, (LPSOCKADDR)&serv, sizeof(serv))==-1);
 	
-	printf("INPUT SOMETHING >> ");
+	printf("INPUT >> ");
 	gets(str);
 	if(strlen(str)==0)strcpy(str, "How are you?");
 	
 	send(sd, str, strlen(str), 0);
 	printf("client-send: %s\n", str);
 	
-	n=recv(sd, str,MAXLINE,0);	//n = 字串長度 
+	n=recv(sd, str, MAXLINE, 0);	//n = 字串長度 
 	str[n] = '\0';
 	printf("client-recv: %s\n", str);		
 
@@ -42,7 +50,7 @@ void main() {
 		connect(sd, (LPSOCKADDR)&serv, sizeof(serv));		
 	/**************************************************/
 		do {
-			printf("INPUT SOMETHING >> ");
+			printf("INPUT >> ");
 			gets(str);
 		}while(strlen(str)==0);
 		if(strcmp(str, "quit")==0||strcmp(str, "exit")==0) break;
--- 2/server-2.c
+++ 2/server-2.c
@@ -7,49 +7,132 @@
 #define MAXLINE 1024
 
 void main() {
-	/**宣告區******************************************/ 
+	/**宣告區**************************************/ 
 	WSADATA wsadata;
 	SOCKET serv_sd, cli_sd;	
 	struct sockaddr_in serv, cli;
 	
-	int cli_len, n, m;			// cli_len:  
-	char str[MAXLINE]; // str:傳輸用暫存 tmp:輸入用暫存 
+	boolean calc = 0;
+	int cli_len, n, step = 0;
+	double x = 0, y = 0, ans = 0; 
+	char str[MAXLINE], tmp[MAXLINE], op; 					// str:傳輸用暫存 tmp:ans用暫存 
 	
-	/**初始化區****************************************/ 
-	WSAStartup(0x101, &wsadata);	// 初始化WSA 
-	serv_sd = socket(AF_INET, SOCK_STREAM, 0);	//初始化(SOCKET)serv_sd
-	//初始化(SOCKET ADDRess)serv 
-	serv.sin_family = AF_INET;
+	/**初始化區************************************/ 
+	WSAStartup(0x101, &wsadata);									// 初始化WSA 
+	serv_sd = socket(AF_INET, SOCK_STREAM, 0);		//初始化(SOCKET)serv_sd								
+	serv.sin_family = AF_INET;										//初始化(SOCKET ADDRess)serv 
 	serv.sin_port = htons(4321);
-	serv.sin_addr.s_addr = 0;	//I'm the localhost!
+	serv.sin_addr.s_addr = 0;											//I'm the localhost!
 	
-	/**工作區*****************************************/ 
+	/**工作區**************************************/ 
 	bind(serv_sd, (LPSOCKADDR)&serv, sizeof(serv));
 		
 	while(1) {
-		listen(serv_sd, 5);	//監聽 (SOCKET)serv_sd 
+		listen(serv_sd, 5);													//監聽 (SOCKET)serv_sd 
 		cli_len = sizeof(cli);
 		printf("Server: waiting for client...\n");
-
-		cli_sd = accept(serv_sd, (LPSOCKADDR)&cli, &cli_len); //連結到serv_sd 的，就是cli_sd，建立通道! 
-		n = recv(cli_sd, str, MAXLINE, 0);	//接收從cli_sd送至serv_sd的字串str；n:str的長度 
-		str[n] = '\0'; //'\0'要自己補上 
+		cli_sd = accept(serv_sd, (LPSOCKADDR)&cli, &cli_len); 
+																								//連結到serv_sd 的，就是cli_sd，建立通道! 
+		n = recv(cli_sd, str, MAXLINE, 0);					//接收從cli_sd送至serv_sd的字串str；n:str的長度 
+		str[n] = '\0'; 															//'\0'要自己補上 
 		printf("Server-recv:  %s\n", str);
 		
-		/**字串處理*****************************************/
-		m = strcmp(str, "How are you?");	//比對 
-		if(m==0) { //0:完全相符 
-			strcpy(str, "Fine, thank you.");
-		}	else {
-			strcpy(str, "What's up?");
+		/**簡易計算機&字串處理***********************/						
+		if(calc) {
+			switch (step) {
+				case 1:
+					x = atof(str);
+					strcpy(str, "\n直接輸入運算符號(+, -, *, /, ^): ");
+					step = 2;
+					break;
+				case 2:
+					if(strcmp(str, "+")==0) {
+						op = '+';
+						strcpy(str, "\n請輸入第二個數字: ");
+					}
+					else if(strcmp(str, "-")==0) {
+						op = '-';
+						strcpy(str, "\n請輸入第二個數字: ");
+					}
+					else if(strcmp(str, "*")==0) {
+						op = '*';
+						strcpy(str, "\n請輸入乘數: ");
+					}
+					else if(strcmp(str, "/")==0) {
+						op = '/';
+						strcpy(str, "\n請輸入分母: ");
+					}
+					else if(strcmp(str, "^")==0) {
+						op = '^';
+						strcpy(str, "\n請輸入次方數: ");
+					}
+					else {
+						strcpy(str, "\n輸入錯誤!\n重新輸入運算符號(+, -, *, /, ^): ");
+						break;
+					}
+					step = 3;
+					break;	
+				case 3:
+					y = atof(str);
+					if(op == '+') {
+						ans = x+y;
+					}
+					else if(op == '-') {
+						ans = x-y;
+					}
+					else if(op == '*') {
+						ans = x*y;
+					}
+					else if(op == '/') {
+						ans = x/y;
+					}
+					else if(op == '^') {
+						ans = 1;
+						int i;
+						for(i=0; i<y; i++) {
+							ans *= x;
+						}
+					}
+					sprintf(tmp, "%f", ans);
+					strcpy(str, "\nAns= ");
+					strcat(str, tmp);
+					strcat(str, "\n要繼續運算輸入\"c\".\n要離開輸入\"q\".");
+					step = 4;
+					break;
+				case 4:
+					if(strcmp(str, "c")==0) {
+						strcpy(str, "\n開始進行，任意二數的四則運算。\n請輸入第一個數字。");
+						ans = 0;
+						step = 1;
+					}else if(strcmp(str, "q")==0) {
+						strcpy(str, "\n離開\n回到一般模式");
+						ans = 0;
+						calc = 0;
+						step = 0;
+					}else {
+						sprintf(tmp, "%f", ans);
+						strcpy(str, "\nAns= ");
+						strcat(str, tmp);
+						strcat(str, "\n要繼續運算輸入\"c\".\n要離開輸入\"q\".");
+					}
+					break;
+			}
+		}else {
+			if(strcmp(str, "How are you?")==0) { 				//比對;0:完全相符 
+				strcpy(str, "Fine, thank you.");
+			}else if(strcmp(str, "calc")==0){
+				strcpy(str, "\nOK!\n開始進行任意二數的四則運算。\n請輸入第一個數字。");
+				calc = 1;
+				step = 1;
+			}else {
+				strcpy(str, "What's up?\n(輸入\"calc\" 進入簡易計算機模式)");
+			}
 		}
-		
-		/**************************************************/
-		send(cli_sd, str, strlen(str), 0); //送字串到cli_sd 
+		/********************************************/
+		send(cli_sd, str, strlen(str), 0); 					//送字串到cli_sd 
 		printf("Server-reply: %s\n", str);
-		
 	}
-	closesocket(cli_sd); //關閉 
+	closesocket(cli_sd); 													//關閉 
 	closesocket(serv_sd);
 	WSACleanup();
 }
