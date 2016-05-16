6b8bd235bb91b30fb3aaa9d4be27952d294c8fd7
晌燁 <a0975129362@gmail.com>
2016-05-16 23:07:58 +0800

    完成作業7-1

--- 7/client-7(udp)(broadcast).c
+++ 7/client-7(udp)(broadcast).c
@@ -5,22 +5,22 @@
 #define MAXLINE 1024
 
 int main(int argc, char** argv) {
-  //宣告======================================
+  //宣告與初始化=====================================
   WSADATA  wsadata;
-  SOCKET  sd;
-  struct sockaddr_in serv, cli;
-  int n, serv_len = sizeof(serv);
+  SOCKET  sd;  // 插頭
+  struct sockaddr_in serv, cli; // 插座
+  int n, serv_len = sizeof(serv), cli_len = sizeof(cli), i;
   char  str[MAXLINE]="";
-  
+
   // 呼叫 WSAStartup() 註冊 WinSock DLL 的使用
   int nResult = WSAStartup(0x101, (LPWSADATA)&wsadata);
   if(nResult!=0) {
     printf("WSA Initialization failed: %d\n", nResult);
     WSACleanup();
-    return;
+    return 0;
   }
-  
-  // 開啟 UDP socket==================================
+
+  // 開啟 UDP socket
   LPHOSTENT hp = (sd = socket(AF_INET, SOCK_DGRAM, 0));
   if(hp == SOCKET_ERROR) {
     printf("get hp error, code: %d\n", WSAGetLastError());
@@ -28,21 +28,35 @@ int main(int argc, char** argv) {
 
   cli.sin_family       = AF_INET;
   cli.sin_addr.s_addr  = 0;
-  cli.sin_port         = htons(1234);
+  cli.sin_port         = htons(5555);
   
-  hp = bind(sd, (LPSOCKADDR) &cli, sizeof(cli));
+  unsigned short port = 0;
+  printf("YOU CAN SET YOUR PORT. (1111~9999, default is 5555.) >>");
+  fgets(str, MAXLINE, stdin);
+  str[strlen(str)-1]='\0'; 
+  if(strlen(str) == 0) {
+    printf("Use default.\n");
+  }else {
+    port = 0;
+    for(i=0; i<4; i++) {
+      port = (u_short)atoi(str);    
+    }
+    cli.sin_port = htons(port);
+  }
+
+  hp = bind(sd, (LPSOCKADDR) &cli, cli_len);
   if(hp < 0) printf("Bind error!\n");
-  
+
   // 工作區========================================
   while(1){
-    n=recvfrom(sd, str, MAXLINE, 0, (LPSOCKADDR)&serv, &serv_len);
+    n = recvfrom(sd, str, MAXLINE, 0, (LPSOCKADDR)&serv, &serv_len);
     str[n]='\0';
-    printf("Server(from %s): %s\n", inet_ntoa(serv.sin_addr), str);
+    printf("Server(from %s:%i): %s\n", inet_ntoa(serv.sin_addr), ntohs(serv.sin_port), str);
   }
   // 關閉 socket
   closesocket(sd);
-  
+
   // 結束 WinSock DLL 的使用
-  WSACleanup();  
+  WSACleanup();
   return 0;
 }
--- 7/server-7(udp)(broadcast).c
+++ 7/server-7(udp)(broadcast).c
@@ -2,41 +2,31 @@
 #include <string.h>
 #include <winsock.h>
 
-#define MAXLINE 1024    /* 字串緩衝區長度 */
-#define clientNum 1
+#define MAXLINE 1024
+#define clientNum 10
 int main() {
-  // 宣告=============================================
+  // 宣告與初始化========================================
   WSADATA wsadata;
   SOCKET  serv_sd;
   struct  sockaddr_in  serv, cli[clientNum];
   int     n, cli_len[clientNum], i, j, k;
   char    str[MAXLINE], tmp[15];
 
-  // 呼叫 WSAStrartup() 註冊 WinSock DLL 的使用=======
+  // 呼叫 WSAStrartup() 註冊 WinSock DLL 的使用
   int nResult = WSAStartup(0x101, (LPWSADATA)&wsadata);
   if(nResult!=0) {
     printf("WSA Initialization failed: %d\n", nResult);
     WSACleanup();
-    return;
+    return 0;
   }
-  
-  // 開啟 UDP socket==================================
+
+  // 開啟 UDP socket
   LPHOSTENT hp = (serv_sd = socket(AF_INET, SOCK_DGRAM, 0));
   if(hp == SOCKET_ERROR) {
     printf("get hp error, code: %d\n", WSAGetLastError());
   }
-  char broadcast = 'a';
-  setsockopt(serv_sd, SOL_SOCKET, SO_BROADCAST, &broadcast, sizeof(broadcast));
   
-  //指定 socket 的 IP 位址和 port number==============
-  
-  cli_len[i] = sizeof(cli[i]); 
-  printf("Server will broadcast.\n");
-  cli[0].sin_family = AF_INET;
-  cli[0].sin_addr.s_addr = inet_addr("10.3.200.127");
-  cli[0].sin_port = htons(1234);
-
-  // 查詢本機資訊=====================================
+  // 查詢本機(Server)資訊=====================================
   printf("What is local host's name & IP?\n");
   hp = gethostname((LPSTR)str, MAXLINE);
   if(hp != 0) {
@@ -48,24 +38,43 @@ int main() {
   if(hp == NULL){
     printf("get hp error, code: %d\n", WSAGetLastError());
   }else {
-    // printf("host name: %s\n", hp->h_name);
-    // printf("host nickname: %s\n", hp->h_aliases[0]);
     printf("host IP: %s\n", inet_ntoa(*(LPIN_ADDR)(hp->h_addr)));
   }
 
+  // 設定廣播功能
+  char broadcast = 'a';
+  setsockopt(serv_sd, SOL_SOCKET, SO_BROADCAST, &broadcast, sizeof(broadcast));
+
+  // 指定 socket 的 IP 位址和 port number
+  // 因為要廣播，所以特別設定client IP。
+  unsigned short port = 0;
+  printf("Server will broadcast.\n");
+  for(i=1; i<clientNum; i++) {
+    cli_len[i] = sizeof(cli[i]);
+    cli[i].sin_family = AF_INET;
+    cli[i].sin_addr.s_addr = inet_addr("255.255.255.255");// 此區網內所有電腦
+    port = 0;
+    for(j=0; j<4; j++) {
+      port = (u_short)(i) + port*10;    
+    }
+    cli[i].sin_port = htons(port);
+    printf("port:%i is OK!\n", ntohs(cli[i].sin_port));
+  }
+  
   // 工作區================================================
   while(1) {
-  	memset(tmp, i%10+'0', sizeof(tmp));
-    sendto(serv_sd, tmp, strlen(tmp), 0, (LPSOCKADDR)&cli[0], cli_len[0]);
-    printf("Server broadcast: %s\n", tmp);// 顯示送去client 的字串
-  	sleep(1);
-	i++;
+    memset(tmp, i%10+'0', sizeof(tmp));
+    for(j=1; j<clientNum; j++) {
+      sendto(serv_sd, tmp, 2+j, 0, (LPSOCKADDR)&cli[j], cli_len[j]);
+      sleep(2/j);
+    }
+    i++;
   }
 
   //========================================================
   closesocket(serv_sd);
-  
+
   //結束 WinSock DLL 的使用=================================
   WSACleanup();
-  return;
+  return 0;
 }
