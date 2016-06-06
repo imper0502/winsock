1c8e93bff49de3b0f3bfc93adcbc17d5516aee26
晌燁 <a0975129362@gmail.com>
2016-06-06 13:50:38 +0800

    update

--- 10-fin/myServer.c
+++ 10-fin/myServer.c
@@ -3,18 +3,18 @@
 // 作者: 吳昱成
 // =============================================================================
 
-// 載入函式庫===================================================================
+// 載入函式庫=====================================================================
 #include <stdio.h>
 #include <string.h>
 #include <winsock.h>
-// 定義巨集=====================================================================
+// 定義巨集======================================================================
 #define CONNECTNUMBER                                                        128
 #define MAXLINE                                                             1024//字串緩衝區長度
-// 主程式=======================================================================
+// 主程式========================================================================
 int main() {
-  // 宣告區&初始化區============================================================
+  // 宣告區&初始化區==============================================================
   WSADATA                                                               wsadata;
-  SOCKET                               tcp_sd, udp_sd, client_sd[CONNECTNUMBER];
+  SOCKET                                  tcp_sd, udp_sd, cli_sd[CONNECTNUMBER];
   struct sockaddr_in                              server, client[CONNECTNUMBER];
   int                                                 client_len[CONNECTNUMBER];
   char                                              str[CONNECTNUMBER][MAXLINE];
@@ -28,18 +28,18 @@ int main() {
     return 0;
   }
   // 開啟 TCP socket
-  hp = (tcp_sd = socket(AF_INET, SOCK_STREAM, 0));
+  LPHOSTENT hp = (tcp_sd = socket(AF_INET, SOCK_STREAM, 0));
   if(hp == SOCKET_ERROR) {
-	printf("get hp error, code: %d\n", WSAGetLastError());
+	  printf("get hp error, code: %d\n", WSAGetLastError());
     fprintf(stderr, "echo@server: Can't open TCP socket.\n");
     return 0;
   }
   // 開啟 UDP socket
-  LPHOSTENT hp = (udp_sd = socket(AF_INET, SOCK_DGRAM, 0));
+  hp = (udp_sd = socket(AF_INET, SOCK_DGRAM, 0));
   if(hp == SOCKET_ERROR) {
-	printf("get hp error, code: %d\n", WSAGetLastError());
-	fprintf(stderr, "echo@server: Can't open UDP socket.\n");
-	return 0;
+	  printf("get hp error, code: %d\n", WSAGetLastError());
+	  fprintf(stderr, "echo@server: Can't open UDP socket.\n");
+	  return 0;
   }
   // 設定廣播功能
   char broadcast = 'a';
@@ -51,25 +51,10 @@ int main() {
   server.sin_addr.s_addr = 0;
   server.sin_port = htons(5554);                                 //"IPPORT_ECHO"
   // 設定client IP & port。
-  client_len[0] = sizeof(client[0]);
   client[0].sin_family = AF_INET;
   client[0].sin_addr.s_addr = inet_addr("255.255.255.255");
   client[0].sin_port = htons(5555);
 
-  // 查詢本機(Server)資訊=======================================================
-  hp = gethostname((LPSTR)str, MAXLINE);
-  if(hp != 0) {
-    printf("get hp error, code: %d\n\n", WSAGetLastError());
-  }else {
-    printf("local host name: %s\n", str);
-  }
-  hp = gethostbyname(str);
-  if(hp == NULL){
-    printf("get hp error, code: %d\n", WSAGetLastError());
-  }else {
-    printf("host IP: %s\n", inet_ntoa(*(LPIN_ADDR)(hp->h_addr)));
-  }
-  
   // 工作區 ====================================================================
   if(bind(tcp_sd, (LPSOCKADDR)&server, sizeof(server)) < 0) {
     fprintf(stderr, "echo@server: Can't bind local address.\n");
@@ -81,6 +66,7 @@ int main() {
     fprintf(stderr, "echo@server: listen() error!!!\n");
     return 0;
   }
+  // 設定client長度
   for(int i; i<CONNECTNUMBER; i++) {
     client_len[i] = sizeof(client[i]);
   }
