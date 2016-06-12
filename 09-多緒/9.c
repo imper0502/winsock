5250766aae1df621d376703a7b990c1b1de034b7
晌燁 <a0975129362@gmail.com>
2016-06-12 22:44:40 +0800

    update #9

--- 09-多緒/9.c
+++ 09-多緒/9.c
@@ -1,35 +1,48 @@
-#include <stdio.h>      /* for printf(), fprintf() */
-#include <winsock.h>    /* for socket(),... */
-#include <stdlib.h>     /* for exit() */
-
-void *ThreadMain(void *arg);             /* Main program of a thread */
-
-/* Structure of arguments to pass to client thread */
-struct ThreadArgs {
-    int clntSock;
+// =============================================================================
+// 最佳的瀏覽效果，請用Notepad++ 開啟。
+// =============================================================================
+#include <stdio.h>                                                              // for printf(), fprintf()
+#include <stdlib.h>                                                             // for exit()
+// Main program of a thread.====================================================
+void *ThreadMain(void *arg);
+// Structure of arguments to pass to client thread.=============================
+struct ThreadArgs
+{
+  int clntSock;
 };
+// Main=========================================================================
+int main(int argc, char *argv[])
+{
+  // 宣告=======================================================================
+  DWORD                                                                threadID;// Thread ID from CreateThread()
+  // Pointer to argument structure for thread
+  struct ThreadArgs                                                 *threadArgs;
+  threadArgs = (struct ThreadArgs *) malloc(sizeof(struct ThreadArgs));
 
-int main(int argc, char *argv[]) {
-    DWORD  threadID;                 /* Thread ID from CreateThread() */
-    struct ThreadArgs *threadArgs;   /* Pointer to argument structure for thread */
-    threadArgs = (struct ThreadArgs *) malloc(sizeof(struct ThreadArgs));
-    threadArgs->clntSock = 5;
-    printf("(Main) Hello World! \n");
-    int nResult = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE) ThreadMain, threadArgs, 0, (LPDWORD)&threadID);
-    if (nResult == NULL) {
-        printf("CreateThread() failed");
-    }
+  threadArgs->clntSock = 2;
 
-    printf("Thread %ld created.\n", threadID);
-    sleep(10);
-    return 0;
+  // TO DO======================================================================
+  printf("(Main) Hello World! \n");
+
+  // Create A NEW Thread.=======================================================
+  int nResult = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE) ThreadMain, threadArgs, 0, (LPDWORD)&threadID);
+  if (nResult == NULL)
+  {
+    printf("CreateThread() failed");
+  }
+
+  printf("Thread %ld created.\n", threadID);
+  sleep(10);
+  return 0;
 }
+// =============================================================================
+void *ThreadMain(void *threadArgs)
+{
+  int arg = ((struct ThreadArgs *) threadArgs)->clntSock;
 
-void *ThreadMain(void *threadArgs) {
-    int arg = ((struct ThreadArgs *) threadArgs)->clntSock;
-    printf("(Thread) Hello World! ");
-    printf(" with arg: %d\n",arg);
-    free(threadArgs);              /* Deallocate memory for argument */
-
-    return (NULL);
+  printf("(Thread) Hello World! ");
+  printf(" with arg: %d\n",arg);
+  // Deallocate memory for argument
+  free(threadArgs);
+  return(NULL);
 }
